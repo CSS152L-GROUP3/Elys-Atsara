9641ae15df79cd4f966309b1e49d00f18ebc44f0
Anna Mikaela Gavieres <155357770+AMBG0215@users.noreply.github.com>
2025-05-22 23:20:39 +0800

    import supabase

---
+++ supabaseClient/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://qjokcvkcibztiibpwqsv.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqb2tjdmtjaWJ6dGlpYnB3cXN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MTk5MzgsImV4cCI6MjA2MzQ5NTkzOH0.zibSsjUbI96fcDCDcOpwnBjW__ep0kzsdCW7dSLVkMs'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
